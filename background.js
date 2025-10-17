5bb3896d47ac6afdd90756887a95f87ddeeaa1b4
henrywisner8 <henrywisner8@gmail.com>
2025-10-17 10:24:33 -0500

    payment URL

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // background.js – handles API calls and upgrade link
 const BACKEND_URL = 'https://scryfall-nlp-backend-production.up.railway.app';
-const PAYMENT_LINK = 'https://buy.stripe.com/14k7sNacaNu0pG8I2kbw0';
+const PAYMENT_LINK = 'https://buy.stripe.com/14A7sM1mCa0W6Mp8Dl2kw00';
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'convert') {
